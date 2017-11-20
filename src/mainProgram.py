384d475a334ede1661b50dadffa2e106c501eb80
mwojdyla <mateow1994@gmail.com>
2017-11-20 21:36:02 +0100

    Common interface for Bayes network classes was created

--- src/mainProgram.py
+++ src/mainProgram.py
@@ -4,25 +4,25 @@ from WekaBayesNetwork import WekaBayesNetwork
 from constants import ALL_ALGORITHMS
 import weka.core.jvm as jvm
 
+
 def main():
     data_provider = DataProvider(test_size=0.2)
     jvm.start()
     trainig_data = data_provider.get_weka_training_data() # for weka
-    weka_bayes_network = WekaBayesNetwork(trainig_data['labels'], ALL_ALGORITHMS['k2'])
-    weka_bayes_network.train_bayes(trainig_data['train_set'])
-    weka_bayes_network.draw_graph()
-    results = weka_bayes_network.predict_and_compare(trainig_data['test_set'])
+    try:
+        weka_bayes_network = WekaBayesNetwork(trainig_data['labels'], ALL_ALGORITHMS['k2'])
+        weka_bayes_network.train_bayes(trainig_data['train_set'])
+        weka_bayes_network.draw_graph()
+        results = weka_bayes_network.predict_and_compare(trainig_data['test_set'])
 
-    # trainig_data = data_provider.get_training_data() # for simple
-    # chow_liu_bayes_network = SimpleBayesNetwork(trainig_data['feature_names'], ALL_ALGORITHMS['chowLiu'])
-    # chow_liu_bayes_network.train_bayes(trainig_data['x_train'], trainig_data['y_train'])
-    # chow_liu_bayes_network.draw_graph()
-    # results = chow_liu_bayes_network.predict_and_compare(trainig_data['x_test'], trainig_data['y_test'])
-
-    print('correct_recurrences:', results['correct_recurrences'])
-    print('correct_no_recurrences:', results['correct_no_recurrences'])
-    print('incorrect_recurrences:', results['incorrect_recurrences'])
-    print('incorrect_no_recurrences:', results['incorrect_no_recurrences'])
+        # trainig_data = data_provider.get_training_data() # for simple
+        # chow_liu_bayes_network = SimpleBayesNetwork(trainig_data['feature_names'], ALL_ALGORITHMS['chowLiu'])
+        # chow_liu_bayes_network.train_bayes(trainig_data['x_train'], trainig_data['y_train'])
+        # chow_liu_bayes_network.draw_graph()
+        # results = chow_liu_bayes_network.predict_and_compare(trainig_data['x_test'], trainig_data['y_test'])
+    except:
+        print('Exception catched! Program will be closed')
+ 
     jvm.stop()
 
 main()
